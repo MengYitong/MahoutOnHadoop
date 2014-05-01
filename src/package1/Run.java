fb2d54d04ca8f1b15aca29bc2dd29867deb7a73c
MengYitong <mengyitongge@163.com>
2014-05-01 16:35:05 +0800

    已经实现"mahout in action" p103  listing 6.5
    此时TestRun3的输出仍是txt，而不是sequencefile

--- src/package1/Run.java
+++ src/package1/Run.java
@@ -6,11 +6,14 @@ public class Run {
 	public static void main(String[] args) throws Exception 
 	{
 		//TestRun.run(args);
-		TestRun3.run(args);
-		/*if(TestRun.run(args)==0)
+		
+		if(TestRun.run(args)==0)
 		{
-			TestRun2.run(args);
-		}*/
+			if(TestRun2.run(args)==0)
+			{
+				TestRun3.run(args);
+			}
+		}
 	}
 	
 }
---
+++ src/package1/UserVectorToCooccurrenceMapper.java
@@ -0,0 +1,29 @@
+package package1;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.mahout.math.VarLongWritable;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+
+public class UserVectorToCooccurrenceMapper extends Mapper<VarLongWritable,VectorWritable,IntWritable,IntWritable>
+{
+	public void map(VarLongWritable userID,VectorWritable userVector,Context context) throws IOException, InterruptedException
+	{
+		Iterator<Vector.Element> it =userVector.get().iterateNonZero();
+		while (it.hasNext())
+		{
+			int index1 = it.next().index();
+			Iterator<Vector.Element> it2 =userVector.get().iterateNonZero();
+			while (it2.hasNext())
+			{
+				int index2 = it2.next().index();
+				context.write(new IntWritable(index1),
+				new IntWritable(index2));
+			}
+		}
+	}
+}
